dff97cab7fe787fc5e08476eb8a2d3b1b1cbaa3c
Sany Islam Shanto <mdsaniislamshanto@gmail.com>
2026-07-31 01:22:44 +0600

    Removed duplicate EmailSettings configuration class

--- Configurations/EmailSettings.cs
+++ Configurations/EmailSettings.cs
@@ -1,15 +1,14 @@
-﻿namespace CRMSystem.Configurations
+﻿namespace CRMSystem.Configurations;
+
+public class EmailSettings
 {
-    public class EmailSettings
-    {
-        public string SenderName { get; set; } = string.Empty;
+    public string SenderName { get; set; } = string.Empty;
 
-        public string SenderEmail { get; set; } = string.Empty;
+    public string SenderEmail { get; set; } = string.Empty;
 
-        public string Password { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 
-        public string Host { get; set; } = string.Empty;
+    public string Host { get; set; } = string.Empty;
 
-        public int Port { get; set; }
-    }
+    public int Port { get; set; }
 }
